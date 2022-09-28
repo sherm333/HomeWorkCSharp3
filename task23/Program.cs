b7a01f03002c191e7663db35e34c8dc124a33ac8
sherm3 <mamafiendaasd@gmail.com>
2022-09-28 13:23:04 +0300

    task23

---
+++ task23/Program.cs
@@ -0,0 +1,37 @@
+﻿/*
+Задача 23
+
+Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+3 -> 1, 8, 27
+5 -> 1, 8, 27, 64, 125
+*/
+
+int ReadData(string line)
+{
+    Console.WriteLine(line);
+    int number = int.Parse(Console.ReadLine() ?? "");
+    return number;
+}
+
+void PrintData(string prefix, string value)
+{
+    Console.WriteLine(prefix + value);
+}
+
+string LineNum(int numN, int p)
+{
+    string outLine = "1";
+    for (int i = 2; i <= numN; i++)
+    {
+        outLine = outLine + "\t" + Math.Pow(i, p);
+    }
+    return outLine;
+}
+
+int numN = ReadData("Введите число N: ");
+string lineTop = LineNum(numN, 1);
+string lineDown = LineNum(numN, 3);
+
+PrintData("", lineTop);
+PrintData("", lineDown);
